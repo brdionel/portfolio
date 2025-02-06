57b9b26c79ebe7564263538ecffb63aecb90878c
brdionel <brunovicente32@gmail.com>
2025-02-06 21:57:41 +0000

    add redirect file into public folder

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  publicDir: 'public',
   plugins: [react()],
 })
