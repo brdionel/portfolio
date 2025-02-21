629df14eeb14dc5b92a1f663a04515d9e9f0b3cc
brdionel <brunovicente32@gmail.com>
2025-02-21 03:58:05 +0000

    add theme based on jekyll-theme-serial-programmer

--- src/componentes/footer/footer.jsx
+++ src/componentes/footer/footer.jsx
@@ -3,6 +3,7 @@ const Footer = () => {
     <>
       <hr className="border-t-[1px] border-primary-light border-b-0 border-l-0 border-r-0 mb-[1.75rem]" />
       <footer>
+      <p className="dark:text-[#A9ABB3]">Theme based on: <a className="text-blue-800" href="https://github.com/sharadcodes/jekyll-theme-serial-programmer" target="_blank">jekyll-theme-serial-programmer</a></p>
         <p className="dark:text-[#A9ABB3]">
           © 2025 | Learning & Growing with React ~ Bruno Dionel Vicente
         </p>
